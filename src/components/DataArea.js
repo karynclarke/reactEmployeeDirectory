e4f3dbc2f8b4b6e8ed55b9d9c66d4830b6d3edce
karynclarke <karynclarke@comcast.net>
2020-10-09 19:31:20 -0700

    component modifications

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -1,108 +1,103 @@
-import React, { useState, useEffect } from "react";
-import DataTable from "./DataTable";
-import Nav from "./Nav";
-import API from "../utils/API";
-import "../styles/DataArea.css";
-import DataAreaContext from "../utils/DataAreaContext";
+import React, { useState, useEffect } from 'react';
+import DataTable from './DataTable';
+import Nav from './Nav';
+import API from '../utils/API';
+import '../styles/DataArea.css';
+import DataAreaContext from '../utils/DataAreaContext';
 
 const DataArea = () => {
-  const [developerState, setDeveloperState] = useState({
-    users: [],
-    order: "descend",
-    filteredUsers: [],
-    headings: [
-      { name: "Image", width: "10%", order: "descend" },
-      { name: "name", width: "10%", order: "descend" },
-      { name: "phone", width: "20%", order: "descend" },
-      { name: "email", width: "20%", order: "descend" },
-          ]
-  });
+	const [developerState, setDeveloperState] = useState({
+		users: [],
+		order: 'descend',
+		filteredUsers: [],
+		headings: [
+			{ name: 'Image', width: '10%', order: 'descend' },
+			{ name: 'name', width: '10%', order: 'descend' },
+			{ name: 'phone', width: '20%', order: 'descend' },
+			{ name: 'email', width: '20%', order: 'descend' }
+		]
+	});
 
-  const handleSort = heading => {
-    let currentOrder = developerState.headings
-      .filter(elem => elem.name === heading)
-      .map(elem => elem.order)
-      .toString();
+	const handleSort = heading => {
+		let currentOrder = developerState.headings
+			.filter(elem => elem.name === heading)
+			.map(elem => elem.order)
+			.toString();
 
-    if (currentOrder === "descend") {
-      currentOrder = "ascend";
-    } else {
-      currentOrder = "descend";
-    }
+		if (currentOrder === 'descend') {
+			currentOrder = 'ascend';
+		} else {
+			currentOrder = 'descend';
+		}
 
-    const compareFnc = (a, b) => {
-      if (currentOrder === "ascend") {
-        // account for missing values
-        if (a[heading] === undefined) {
-          return 1;
-        } else if (b[heading] === undefined) {
-          return -1;
-        }
-        // numerically
-        else if (heading === "name") {
-          return a[heading].first.localeCompare(b[heading].first);
-         } else {
-          return a[heading].localeCompare(b[heading]);
-        }
-      } else {
-        // account for missing values
-        if (a[heading] === undefined) {
-          return 1;
-        } else if (b[heading] === undefined) {
-          return -1;
-        }
-      }
-    };
+		const compareFnc = (a, b) => {
+			if (currentOrder === 'ascend') {
+				// account for missing values
+				if (a[heading] === undefined) {
+					return 1;
+				} else if (b[heading] === undefined) {
+					return -1;
+				}
+				// numerically
+				else if (heading === 'name') {
+					return a[heading].first.localeCompare(b[heading].first);
+				} else {
+					return a[heading].localeCompare(b[heading]);
+				}
+			} else {
+				// account for missing values
+				if (a[heading] === undefined) {
+					return 1;
+				} else if (b[heading] === undefined) {
+					return -1;
+				}
+			}
+		};
 
-    const sortedUsers = developerState.filteredUsers.sort(compareFnc);
-    const updatedHeadings = developerState.headings.map(elem => {
-      elem.order = elem.name === heading ? currentOrder : elem.order;
-      return elem;
-    });
+		const sortedUsers = developerState.filteredUsers.sort(compareFnc);
+		const updatedHeadings = developerState.headings.map(elem => {
+			elem.order = elem.name === heading ? currentOrder : elem.order;
+			return elem;
+		});
 
-    setDeveloperState({
-      ...developerState,
-      filteredUsers: sortedUsers,
-      headings: updatedHeadings
-    });
-  };
+		setDeveloperState({
+			...developerState,
+			filteredUsers: sortedUsers,
+			headings: updatedHeadings
+		});
+	};
 
-  const handleSearchChange = event => {
-    console.log ("developerState", developerState.users)
-    const filter = event.target.value;
-    const filteredList = developerState.users.filter(item => {
-      let values = item.name.first.toLowerCase() + " " + item.name.last.toLowerCase();
-      
-    if(values.indexOf(filter.toLowerCase()) !== -1){
-      
-       return item
-    }
-    else {return null} 
-    });
-console.log("filteredList", filteredList)
-    setDeveloperState({ ...developerState, filteredUsers: filteredList });
-  };
-console.log ("developerState", developerState)
-    useEffect(() => {
-    API.getUsers().then(results => {
-      
-      setDeveloperState({
-        ...developerState,
-        users: results.data.results,        
-      });
-    });
-  }, []);
+	const handleSearchChange = event => {
+		// console.log ("developerState", developerState.users)
+		const filter = event.target.value;
+		const filteredList = developerState.users.filter(item => {
+			let values = item.name.first.toLowerCase() + ' ' + item.name.last.toLowerCase();
 
-  return (
-    <DataAreaContext.Provider
-      value={{ developerState, handleSearchChange, handleSort }}
-    >
-      <Nav />
-      <div className="data-area">
-        {developerState.filteredUsers.length > 0 ? <DataTable /> : <div></div>}
-      </div>
-    </DataAreaContext.Provider>
-  );
+			if (values.indexOf(filter.toLowerCase()) !== -1) {
+				return item;
+			} else {
+				return null;
+			}
+		});
+		console.log('filteredList', filteredList);
+		setDeveloperState({ ...developerState, filteredUsers: filteredList });
+	};
+	// console.log ("developerState", developerState)
+	useEffect(() => {
+		API.getUsers().then(results => {
+			setDeveloperState({
+				...developerState,
+				users: results.data.results
+			});
+		});
+	}, [developerState]);
+
+	return (
+		<DataAreaContext.Provider value={{ developerState, handleSearchChange, handleSort }}>
+			<Nav />
+			<div className="data-area">{developerState.filteredUsers.length > 0 ? <DataTable /> : <div></div>}</div>
+		</DataAreaContext.Provider>
+	);
 };
 
 export default DataArea;
